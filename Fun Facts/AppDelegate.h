f0fc51c64e50c302c5f757b8ed313f14feb436d0
Erik Nilsen <enilsen16@live.com>
2015-04-14 14:30:20 -0700

    Initial Commit

---
+++ Fun Facts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fun Facts
+//
+//  Created by Erik Nilsen on 4/14/15.
+//  Copyright (c) 2015 Erik Nilsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fun Facts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fun Facts
+//
+//  Created by Erik Nilsen on 4/14/15.
+//  Copyright (c) 2015 Erik Nilsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
